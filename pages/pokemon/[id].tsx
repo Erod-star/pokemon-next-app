a344aeb3bd4a02599dafe1cd8bd44e7acb0acc2d
Edson <erodstardev@gmail.com>
2022-08-17 12:10:45 -0500

    Add daily refresh and dynamic pettitions

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -122,17 +122,30 @@ export const getStaticPaths: GetStaticPaths<Params> = async () => {
 
   return {
     paths,
-    fallback: false,
+    // fallback: false,
+    fallback: "blocking",
   };
 };
 
 export const getStaticProps: GetStaticProps = async ({ params }) => {
   const { id } = params as { id: string };
 
+  const pokemon = await getPokemonInfo(id);
+
+  if (!pokemon) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+
   return {
     props: {
-      pokemon: await getPokemonInfo(id),
+      pokemon,
     },
+    revalidate: 86400, // En segundos
   };
 };
 
